ac3a2be54fca2b894f42b1e2135791bb8ef4cdfd
alaba-web <aminualaba9@gmail.com>
2022-12-22 06:15:04 -0800

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _strncpy: a function that copy strings.
+ * _strncpy - a function that copy strings.
  * @dest: an input string.
  * @src: an input string.
  * @n: an input integer.
