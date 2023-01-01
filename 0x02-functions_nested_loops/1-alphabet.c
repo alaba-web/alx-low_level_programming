b93c98d93b6542f8b6e689d8ff626624fbaee75c
alaba-web <aminualaba9@gmail.com>
2023-01-01 14:23:52 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_alphabet - It checks description
+ * Description: It prints the alphabets in lowercase and new line follows
+ * Return: Nothing
+ */
+void print_alphabet(void)
+{
+	char a;
+	
+	for (a = 'a'; a <= 'z'; a++)
+		_putchar(a);
+	_putchar('\n');
+}
